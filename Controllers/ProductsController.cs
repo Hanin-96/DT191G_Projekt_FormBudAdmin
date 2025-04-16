a969131a90494ce509a7c0fb07d0594f27364454
Hanin Farhan <haninfarhan96@gmail.com>
2025-04-16 22:23:47 +0200

    Rensat kod och lagt till authorization på skyddade delar

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FormBudAdmin.Data;
 using FormBudAdmin.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FormBudAdmin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _context;
